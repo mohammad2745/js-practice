5cf766b14f480608305939eb1862c9a85584c0f3
mohammad2745 <mohammadfcc2745@gmail.com>
2023-03-20 01:17:28 +0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+class Rectangle {
+    constructor() {
+        console.log("The Rectangle is being created");
+    }
+}
+
+// constructor is called when object is created.
+let myRectangle = new Rectangle();
