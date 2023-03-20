5d2c15e7bae041daaa65d7818753b48c416ac349
mohammad2745 <mohammadfcc2745@gmail.com>
2023-03-20 20:00:38 +0600

    Promises and Promis all

---
+++ promise.js
@@ -0,0 +1,44 @@
+const posts = [
+    { title: 'Post one', body: 'This is post one' },
+    { title: 'Post two', body: 'This is post two' }
+];
+
+function getPosts() {
+    setInterval(() => {
+        let output = '';
+        posts.forEach((post, index) => {
+            output += `<li>${index}--${post.title}--${post.body}</li>`
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+function createPost(post) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+
+            const error = false;
+
+            if (!error) {
+                resolve();
+            } else {
+                reject('Error!! Something went wrong');
+            }
+        }, 2000);
+    });
+
+}
+
+// createPost({ title: 'Post three', body: 'This is post three' })
+
+// .then(getPosts)
+//     .catch(err => console.log(err));
+
+// Promise.all
+const promise1 = Promise.resolve('Hello World');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) =>
+    setTimeout(resolve, 2000, 'Goodbye'));
+
+Promise.all([promise1, promise2, promise3]).then((values) => console.log(values));
